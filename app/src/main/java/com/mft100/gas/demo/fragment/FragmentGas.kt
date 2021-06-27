432d25a3f5ee0bdeb7b8a060e678aae76cf443a6
sollyu <sollyu@qq.com>
2021-06-28 01:36:22 +0800

    默认变更列表

--- app/src/main/java/com/mft100/gas/demo/fragment/FragmentGas.kt
+++ app/src/main/java/com/mft100/gas/demo/fragment/FragmentGas.kt
@@ -1,24 +1,27 @@
 package com.mft100.gas.demo.fragment
 
 import android.content.Context
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.widget.AppCompatImageButton
+import androidx.slidingpanelayout.widget.SlidingPaneLayout
 import com.google.gson.Gson
 import com.mft100.gas.demo.R
 import com.mft100.gas.demo.databinding.FragmentGasBinding
 import com.mft100.gas.demo.fragment.gas.pojo.GasPojoSummary
+import com.qmuiteam.qmui.util.QMUIDisplayHelper
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.scopes.FragmentScoped
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.observers.DisposableSingleObserver
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.observers.DisposableObserver
 import io.reactivex.rxjava3.schedulers.Schedulers
 import org.jetbrains.annotations.NotNull
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.lang.ref.SoftReference
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @FragmentScoped
@@ -46,18 +49,29 @@ internal class FragmentGas : FragmentBase(), View.OnClickListener {
         mViewBinding.btn002.setOnClickListener(this)
         mViewBinding.btn003.setOnClickListener(this)
 
-        Single.just(Unit)
+        val orientation = context.resources.configuration.orientation
+        val layoutParams = mViewBinding.mainContent.layoutParams as SlidingPaneLayout.LayoutParams
+        layoutParams.marginStart = if (orientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) 0 else QMUIDisplayHelper.dp2px(context, 63)
+        mViewBinding.mainContent.layoutParams = layoutParams
+
+        Observable.just(300L, 1000L)
             .subscribeOn(Schedulers.io())
-            .delay(300, TimeUnit.MILLISECONDS)
-            .map { mViewBinding.slidingPaneLayout.openPane() }
-            .delay(1, TimeUnit.SECONDS)
-            .map { mViewBinding.slidingPaneLayout.closePane() }
+            .map { Thread.sleep(it) }
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : DisposableSingleObserver<Boolean>() {
-                override fun onSuccess(t: Boolean) {
+            .subscribe(object : DisposableObserver<Unit>() {
+                override fun onNext(t: Unit) {
+                    if (mViewBinding.slidingPaneLayout.isOpen)
+                        mViewBinding.slidingPaneLayout.closePane()
+                    else
+                        mViewBinding.slidingPaneLayout.openPane()
+                }
+
+                override fun onComplete() {
+                    mViewBinding.btn001.performClick()
                 }
 
                 override fun onError(e: Throwable?) {
+                    logger.error("LOG:FragmentGas:onError", e)
                 }
             })
     }
@@ -69,6 +83,15 @@ internal class FragmentGas : FragmentBase(), View.OnClickListener {
             mViewBinding.btn002 -> onClickLeftButton002(view)
             mViewBinding.btn003 -> onClickLeftButton003(view)
         }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        val context: Context = requireActivity()
+        logger.info("LOG:FragmentGas:onResume:orient={} ", newConfig.orientation)
+        val s = mViewBinding.mainContent.layoutParams as SlidingPaneLayout.LayoutParams
+        s.marginStart = if (newConfig.orientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) 0 else QMUIDisplayHelper.dp2px(context, 63)
+        mViewBinding.mainContent.layoutParams = s
     }
 
     private val logger: Logger = LoggerFactory.getLogger(this.javaClass)
